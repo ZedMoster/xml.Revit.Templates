102e50face288ffc614a0b97cd2e625251768725
ZedMoster <ZedMoster1@gmail.com>
2024-10-28 15:33:35 +0800

    更新版本支持2018~2025

--- Readme.md
+++ Readme.md
@@ -7,6 +7,8 @@
 
 该模板包含用于快速创建 Revit 附加命令的项目模板
 
+默认支持Revit版本18~25
+
 推荐使用拓展包 `https://www.nuget.org/packages/xml.Revit.Toolkit`
 
 ## Installation
--- source/xml.Revit.AddIn/Cmd.cs
+++ source/xml.Revit.AddIn/Cmd.cs
@@ -1,6 +1,6 @@
 ﻿namespace xml.Revit.AddIn
 {
-    [Xml("输入功能名称")]
+    [Xml("输入功能名称", "功能提示", "功能长提示")]
     [Transaction(TransactionMode.Manual)]
     public class Cmd : XmlExternalCommand
     {
