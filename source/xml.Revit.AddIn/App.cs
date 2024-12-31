2c8e6b0bd3e661972db39cf0f82e89108d6d1e2d
ZedMoser <ZedMoster1@gmail.com>
2024-12-31 11:04:19 +0800

    更新版本1.6.0

--- source/xml.Revit.AddIn/App.cs
+++ source/xml.Revit.AddIn/App.cs
@@ -1,6 +1,6 @@
 namespace xml.Revit.AddIn
 {
-    public sealed class Application : XmlExternalApplication
+    public sealed class App : XmlExternalApplication
     {
         public override string PanelName => "Revit二次开发教程";
     }
